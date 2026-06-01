f13dc1c657359ebf6bef7825c1d498f2124cae0a
loharaws <127829795+loharaws@users.noreply.github.com>
2026-06-01 18:58:51 +0530

    Update init.sql

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,18 +1,6 @@
--- 1. Create database
 CREATE DATABASE IF NOT EXISTS crud_app;
-
--- 2. Create user (important for real environments)
-CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin123';
-
--- 3. Grant permissions
-GRANT ALL PRIVILEGES ON crud_app.* TO 'admin'@'%';
-
-FLUSH PRIVILEGES;
-
--- 4. Use database
 USE crud_app;
 
--- 5. Create tables
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
